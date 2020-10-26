16839ad3b13ab011fbe8b27c1092a649f2c5300b
Mateo Gutiérrez Melo <72769799+MateoG404@users.noreply.github.com>
2020-10-26 17:05:59 -0500

    Update Ventana.java

--- src/Interfaz/Ventana.java
+++ src/Interfaz/Ventana.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Interfaz;
 
 /**
  *
- * @author John Silva
+ * @author John Silva, MateoG404 and LauraArdila
  */
 
 import Mundo.Empresa;
