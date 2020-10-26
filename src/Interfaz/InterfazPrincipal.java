87f2410f2fc33fd53a57c3d1df0cd7fea80b1899
Mateo Gutiérrez Melo <72769799+MateoG404@users.noreply.github.com>
2020-10-26 17:04:49 -0500

    Update InterfazPrincipal.java

--- src/Interfaz/InterfazPrincipal.java
+++ src/Interfaz/InterfazPrincipal.java
@@ -13,7 +13,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 /**
  *
- * @author John Silva
+ * @author John Silva, MateoG404 and Laura Ardila
  */
 public class InterfazPrincipal extends javax.swing.JFrame {
 
